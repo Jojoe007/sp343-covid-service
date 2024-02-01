d3876a5a09c82845d4dc57336367bcdc6318ff58
Takdanai <jozaza07@gmail.com>
2024-02-01 20:30:56 +0700

    Initial Commit

---
+++ src/main/java/dev/takdanai/covid/CovidServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.takdanai.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidServiceApplication.class, args);
+    }
+
+}
